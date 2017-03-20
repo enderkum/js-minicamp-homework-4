34b8b23e9902c630dc9cc009d5990d2663ed70f4
enderkum <enkum@hotmail.com>
2017-03-20 10:10:32 +0300

    Homework done

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* Callback Functions
-	* Closure
-	* arguments array
-	* recursion
-	* prototype
-	* constructors
+	* Callback Functions : Functions that are called (or executed) inside other functions where they are passed to as parameters.
+	* Closure : Function returned from another function, so that it can remember the value of variables created inside their parent function and can also change this value. As closures can be invoked outside their parent function, initial values of variables in their parent function are not considered at subsequent calls.
+	* arguments array : In javascript, arguments object is an Array-like object corresponding to the arguments passed to a function. So, items of arguments object can be reached like arrays.
+	* recursion : Recursion is a technique for iterating over an operation by having a function call itself repeatedly until it arrives at a result. 
+	* prototype : All JavaScript objects inherit their properties and methods from their prototype which is also an object.
+	* constructors : It can be considered as a template that we can use for all of the different objects that we want to create. We can add functions to constructors to process the values of created objects.
 
 
 2. Fork and clone this repo.  When you need to commit use the following commands.
--- exercises.js
+++ exercises.js
@@ -3,33 +3,65 @@
 //Do not change any of the function names
 
 function multiplyArguments() {
+	if (!arguments.length) return 0;
+	var product = 1;
+	for (var i = 0; i < arguments.length; i++){
+		product *= arguments[i];
+	}
+  return product;
 	//use the arguments keyword to multiply all of the arguments together and return the product
 	//if no arguments are passed in return 0
 	//if one argument is passed in just return it
 }
 
 function invokeCallback(cb) {
+	cb();
 	//invoke cb
 }
 
 function sumArray(numbers, cb) {
+	var sum = 0;
+	for (var i = 0; i <numbers.length; i++) {
+		sum += numbers[i];
+	}
+	cb(sum);
 	//sum up all of the integers in the numbers array
 	//pass the result to cb
 	//no return is necessary
 }
 
 function forEach(arr, cb) {
+	arr.forEach(function(num,i){
+		cb(num);
+	});
 	//iterate over arr and pass its values to cb one by one
 	//hint: you will be invoking cb multiple times (once for each value in the array)
 }
 
 function map(arr, cb) {
+	
+	var newArr = arr.map(function(num){
+		return cb(num);
+	});
+return newArr;
 	//create a new array
 	//iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
 	//the new array should be the same length as the array argument
 }
 
 function getUserConstructor() {
+	function user(options) {
+		this.username = options.username;
+		this.name = options.name;
+		this.email = options.email;
+		this.password = options.password;
+	}
+
+	user.prototype.sayHi = function() {
+		return 'Hello, my name is' + this.name;
+	};
+
+	return user;
 	//create a constructor called User
 	//it should accept an options object with username, name, email, and password properties
 	//in the constructor set the username, name, email, and password properties
@@ -39,11 +71,23 @@ function getUserConstructor() {
 }
 
 function addPrototypeMethod(Constructor) {
+	Constructor.prototype.sayHi = function() {
+		return 'Hello World!';
+	};
 	//add a method to the constructor's prototype
 	//the method should be called 'sayHi' and should return the string 'Hello World!'
 }
 
 function addReverseString() {
+	
+	String.prototype.reverse = function() {		
+			var reverse = [];
+			for (var i = this.length - 1; i > -1; i--){
+				reverse += this[i];
+			}
+			return reverse;				
+	};
+	
 	//add a method to the string constructor's prototype that returns a reversed copy of the string
 	//name this method reverse
 	//hint:
@@ -51,6 +95,8 @@ function addReverseString() {
 }
 
 function nFactorial(n) {
+	if (n === 1) return 1;
+	return n * nFactorial(n - 1);
 	//return the factorial for n
 	//solve this recursively
 	//example:
@@ -58,6 +104,11 @@ function nFactorial(n) {
 }
 
 function cacheFunction(cb) {
+
+	var cache = {};
+	return function cached(arg) {
+		return cache.hasOwnProperty(arg) ? cache[arg] : cache[arg] = cb(arg); 
+	};
 	//Extra Credit
 	//use closure to create a cache for the cb function
 	//the function that you return should accept a single argument and invoke cb with that argument
